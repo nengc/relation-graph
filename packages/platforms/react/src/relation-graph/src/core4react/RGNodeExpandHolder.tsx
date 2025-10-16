7bd9d7fef83505783699038c6cae00a5bdac907f
nengc <894870246@qq.com>
2025-10-16 13:00:17 +0800

    Refactor event handling for expand/collapse actions

--- packages/platforms/react/src/relation-graph/src/core4react/RGNodeExpandHolder.tsx
+++ packages/platforms/react/src/relation-graph/src/core4react/RGNodeExpandHolder.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import {RGNodeExpandHolderProps} from "../../../types-react";
 
 const RGNodeExpandHolder: React.FC<RGNodeExpandHolderProps> = ({expandButtonClass, expandOrCollapseNode, expandHolderPosition, color}) => {
+  const handleAction = (e: React.MouseEvent | React.TouchEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    expandOrCollapseNode(e);
+  };
+
+  // 检测是否为触摸设备
+  const isTouchDevice = typeof window !== 'undefined' && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
+  
   return (
     <div
       className={`c-expand-positon-${expandHolderPosition} c-btn-open-close`}
@@ -9,12 +18,8 @@ const RGNodeExpandHolder: React.FC<RGNodeExpandHolderProps> = ({expandButtonClas
       <span
         className={expandButtonClass}
         style={{ backgroundColor: color }}
-        onClickCapture={(e) => {
-          expandOrCollapseNode(e);
-        }}
-        onTouchEnd={(e) => {
-          expandOrCollapseNode(e);
-        }}
+        onClick={!isTouchDevice ? handleAction : undefined}
+        onTouchEnd={isTouchDevice ? handleAction : undefined}
       ></span>
     </div>
   );
